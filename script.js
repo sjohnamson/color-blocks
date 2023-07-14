9c754976aa7dd4e222f0c96d5fe5580b66e0916e
Sam Johnson <s_johnso@yahoo.com>
2023-07-13 19:57:16 -0500

    cleaned formatting

--- script.js
+++ script.js
@@ -4,27 +4,27 @@ $(document).ready(onReady)
 
 
 function onReady() {
-// create handler for buttons to reference addBlock functions and count functions
-$('#red-btn').on('click', addRedBlock).on('click', countRed);
+    // create handler for buttons to reference addBlock functions and count functions
+    $('#red-btn').on('click', addRedBlock).on('click', countRed);
 
 
-$('#blue-btn').on('click', addBlueBlock).on('click', countBlue);
+    $('#blue-btn').on('click', addBlueBlock).on('click', countBlue);
 
 
-$('#green-btn').on('click', addGreenBlock).on('click', countGreen);
+    $('#green-btn').on('click', addGreenBlock).on('click', countGreen);
 
 
-$('#yellow-btn').on('click', addYellowBlock).on('click', countYellow);
+    $('#yellow-btn').on('click', addYellowBlock).on('click', countYellow);
 
 
-// create handler for buttons  to reference deleteBlock functions and subtractCount functions
-$('#blocks').on('click', '.red-fill', deleteRed).on('click', '.red-fill', subtractRedCount);
+    // create handler for buttons  to reference deleteBlock functions and subtractCount functions
+    $('#blocks').on('click', '.red-fill', deleteRed).on('click', '.red-fill', subtractRedCount);
 
-$('#blocks').on('click', '.blue-fill', deleteBlue).on('click', '.blue-fill', subtractBlueCount);
+    $('#blocks').on('click', '.blue-fill', deleteBlue).on('click', '.blue-fill', subtractBlueCount);
 
-$('#blocks').on('click', '.green-fill', deleteGreen).on('click', '.green-fill', subtractGreenCount);
+    $('#blocks').on('click', '.green-fill', deleteGreen).on('click', '.green-fill', subtractGreenCount);
 
-$('#blocks').on('click', '.yellow-fill', deleteYellow).on('click', '.yellow-fill', subtractYellowCount);
+    $('#blocks').on('click', '.yellow-fill', deleteYellow).on('click', '.yellow-fill', subtractYellowCount);
 
 }
 
@@ -56,25 +56,25 @@ function addYellowBlock() {
 // functions to add to the color block counters
 function countRed() {
     $('#red-count').html(function (i, val) {
-        return val * 1 +1 
+        return Number(val) + 1
     });
 }
 
 function countBlue() {
     $('#blue-count').html(function (i, val) {
-        return val * 1 +1 
+        return Number(val) + 1
     });
 }
 
 function countGreen() {
     $('#green-count').html(function (i, val) {
-        return val * 1 +1 
+        return Number(val) + 1
     });
 }
 
 function countYellow() {
     $('#yellow-count').html(function (i, val) {
-        return val * 1 +1 
+        return Number(val) + 1
     });
 }
 
@@ -99,24 +99,24 @@ function deleteYellow() {
 // Functions to subtract the block counters
 function subtractRedCount() {
     $('#red-count').html(function (i, val) {
-        return val * 1 - 1 
+        return val - 1
     });
 }
 
 function subtractBlueCount() {
     $('#blue-count').html(function (i, val) {
-        return val * 1 - 1 
+        return val - 1
     });
 }
 
 function subtractGreenCount() {
     $('#green-count').html(function (i, val) {
-        return val * 1 - 1 
+        return val - 1
     });
 }
 
 function subtractYellowCount() {
     $('#yellow-count').html(function (i, val) {
-        return val * 1 - 1 
+        return val - 1
     });
 }
